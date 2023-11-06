7b4e3474908677cea3db2fd0f0b285a7f64eb087
Asdmitruk <dmitrukadrian@gmail.com>
2023-11-06 23:49:59 +0300

    fix not tasks

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -8,13 +8,16 @@ import { fetchTodo } from "../redux/todo/async-actions";
 import { MainCalendar } from "../components/MainCalendar";
 import { useSelector } from "react-redux";
 import { selectTodoData } from "../redux/todo/selectors";
-import { Spin } from "antd";
+import { Button, Spin } from "antd";
 import { Status } from "../redux/auth/types";
 
 import cn from "classnames";
+import { useNavigate } from "react-router-dom";
 
 export const MainPage: FC = () => {
   const { data, status } = useSelector(selectTodoData);
+
+  const navigate = useNavigate();
 
   const dispatch = useAppDispatch();
 
@@ -22,6 +25,10 @@ export const MainPage: FC = () => {
     dispatch(fetchUser());
     dispatch(fetchTodo({}));
   }, [dispatch]);
+
+  const handleAddTask = () => {
+    navigate("/add");
+  };
 
   return (
     <div className="container">
@@ -32,13 +39,26 @@ export const MainPage: FC = () => {
 
         <div
           className={cn("mainContent", {
-            ["mainContentSpiner"]: status === Status.LOADING,
+            ["mainContentSpiner"]: status === Status.LOADING || !data.length,
           })}
         >
           {status === Status.LOADING ? (
             <Spin size="large" />
           ) : (
             data.map((elem) => <Card key={elem._id} elem={elem} />)
+          )}
+
+          {!data.length && status === Status.SECCESS && (
+            <div className="mainNot">
+              <h2 className="mainNotTitle">Новых задача пока нет</h2>
+              <Button
+                onClick={handleAddTask}
+                className="headerBtn"
+                type="primary"
+              >
+                Добавить задачу
+              </Button>
+            </div>
           )}
         </div>
       </div>
